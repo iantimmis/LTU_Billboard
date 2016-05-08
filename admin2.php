1d589b38945553618062bf84bf67b82bd6dd8756
katnapier <knapier@ltu.edu>
2016-05-08 15:18:23 -0400

    Main admin file changes
    1.)Removed javascript.
    2.)Included an external js and php file.
    3.)Altered searchbox area to become a form.
    4.)Added links to an add/edit announcements modal and a view organizations modal.

--- admin2.php
+++ admin2.php
@@ -26,6 +26,8 @@
 	$loggedIn = $loggedInAsOrg || $loggedInAsUser;
 ?>
 
+<?php require 'adminModalFunctions.php';?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -48,233 +50,8 @@
 				$(".chooseActType").hide();
 				$("#"+target).show();
 		});});
-      var currPage=0
-      var maxPage=0
-      var sortBy=0
-      var type=0
-      function onLoad()
-      {
-          baseFunction("findMax.php", findMaxPage);
-          baseFunction("buildRequestTable.php?page="+currPage+"&sort="+sortBy+"&type="+type, showRequests);
-      }
-      function next()
-      {
-          if(currPage==maxPage)
-          {
-              //do nothing
-          }
-          else
-          {
-              currPage++;
-              baseFunction("buildRequestTable.php?page="+currPage+"&sort="+sortBy+"&type="+type, showRequests);
-          }
-      }
-      function prev()
-      {
-          if(currPage==0)
-          {
-              //do nothing
-          }
-          else
-          {
-              currPage--;
-              baseFunction("buildRequestTable.php?page="+currPage+"&sort="+sortBy+"&type="+type, showRequests);
-          }
-      }
-      function sortByRequestDate()
-      {
-          if(sortBy==0)
-          {
-              //do nothing
-          }
-          else
-          {
-              sortBy=0;
-
-              document.getElementById("sortDate").style.backgroundColor="rgba(255, 255, 255, .3)";
-              document.getElementById("sortDate").style.color="#003d78";
-              document.getElementById("sortRecent").style.backgroundColor="#3385ff";
-              document.getElementById("sortRecent").style.color="white";
-
-              baseFunction("buildRequestTable.php?page="+0+"&sort="+sortBy+"&type="+type, showRequests);
-          }
-        
-      }
-      function sortByEventDate()
-      {
-          if(sortBy==1)
-          {
-              //do nothing
-          }
-          else
-          {
-              sortBy=1;
-
-              document.getElementById("sortRecent").style.backgroundColor="rgba(255, 255, 255, .3)";
-              document.getElementById("sortRecent").style.color="#003d78";
-              document.getElementById("sortDate").style.backgroundColor="#3385ff";
-              document.getElementById("sortDate").style.color="white";
-
-              baseFunction("buildRequestTable.php?page="+0+"&sort="+sortBy+"&type="+type, showRequests);
-          }
-      }
-      function showEventRequests()
-      {
-          if(type==0)
-          {
-              //do nothing
-          }
-          else
-          {
-              type=0;
-
-              document.getElementById("orgReq").style.backgroundColor="rgba(255, 255, 255, .3)";
-              document.getElementById("orgReq").style.color="#003d78";
-              document.getElementById("eventReq").style.backgroundColor="#3385ff";
-              document.getElementById("eventReq").style.color="white";
-
-              
-              document.getElementById("sortDate").style.backgroundColor="rgba(255, 255, 255, .3)";
-              document.getElementById("sortDate").style.color="#003d78";
-              document.getElementById("sortDate").style.display="initial";
-              document.getElementById("sortRecent").style.backgroundColor="#3385ff";
-              document.getElementById("sortRecent").style.color="white";
-              
-
-              baseFunction("buildRequestTable.php?page="+0+"&sort="+sortBy+"&type="+type, showRequests);
-          }
-      }
-      function showOrgRequests()
-      {
-          if(type==1)
-          {
-              //do nothing
-          }
-          else
-          {
-              type=1;
-
-              document.getElementById("eventReq").style.backgroundColor="rgba(255, 255, 255, .3)";
-              document.getElementById("eventReq").style.color="#003d78";
-              document.getElementById("orgReq").style.backgroundColor="#3385ff";
-              document.getElementById("orgReq").style.color="white";
-
-              document.getElementById("sortDate").style.display="none";
-              document.getElementById("sortRecent").style.backgroundColor="#3385ff";
-              document.getElementById("sortRecent").style.color="white";
-
-              baseFunction("buildRequestTable.php?page="+0+"&sort="+sortBy+"&type="+type, showRequests);
-          }
-      }
-      function baseFunction(url, nameOfFunction)
-      {
-          var xhttp=new XMLHttpRequest();
-          xhttp.onreadystatechange=
-              function()
-              {
-                  if(xhttp.readyState==4 && xhttp.status==200)
-                  {
-                      nameOfFunction(xhttp);
-                  }
-              };
-              xhttp.open("GET", url, true);
-              xhttp.send();
-      }
-      function baseFunctionPost(url, nameOfFunction, msg)
-      {
-          var xhttp=new XMLHttpRequest();
-          xhttp.onreadystatechange=
-              function()
-              {
-                  if(xhttp.readyState==4 && xhttp.status==200)
-                  {
-                      nameOfFunction(xhttp);
-                  }
-              };
-              xhttp.open("POST", url, true);
-              xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-              xhttp.send(msg);
-      }
-      function findMaxPage(xhttp)
-      {
-          maxPage=xhttp.responseText;   
-      }
-      function showRequests(xhttp)
-      {
-          document.getElementById("requestTable").innerHTML=xhttp.responseText;        
-      }
-      function handleAcceptance(xhttp)
-      {
-          document.getElementById("acceptanceResponse").innerHTML=xhttp.responseText;
-      }
-      function accept(acc)
-      {
-          var arr=document.getElementsByTagName("INPUT");  
-          var checkedIt=new Array();
-          for(var i=0; i<arr.length; i++)
-          {
-              if(arr[i].className=="checks" && arr[i].checked)
-              {
-                  var rowID=arr[i].id;
-                  checkedIt.push(rowID);
-              }
-          }
-          if(checkedIt==="undefined" || checkedIt.length==0)
-          {
-              return;
-          }
-          if(acc==0)
-          {
-              var decision=confirm("Are you sure you want to delete request(s)?");
-              if(!decision)
-              {
-	          return;
-              }	
-          }
-          else
-          {
-              var decision=confirm("Are you sure you want to accpet request(s)?");
-              if(!decision)
-              {
-	          return;
-              }	
-          }
-          var checkedItems=new Array();
-          for(var j=0; j<checkedIt.length; j++)
-          {
-              /*var val=0;*/
-              var val=document.getElementById("reqs").rows[checkedIt[j]].cells[0].innerHTML;
-              checkedItems.push(val);
-          }
-          var data="a="+acc+"&type="+type+"&data="+checkedItems;
-          baseFunctionPost("acceptOrDecline.php", handleAcceptance, data); 
-          baseFunction("buildRequestTable.php?page="+currPage+"&sort="+sortBy+"&type="+type, showRequests); 
-      }
-      function changeAll()
-      {
-          var arr=document.getElementsByTagName("INPUT");       
-          if(document.getElementById("masterCheck").checked)  
-          {			
-              for(var i=0; i<arr.length; i++)
-              {
-                  if(arr[i].className=="checks" && !arr[i].checked)
-                  {
-                      arr[i].checked=true;
-                  }
-              }
-          }
-          else 
-          {
-              for(var i=0; i<arr.length; i++)
-              {
-                  if(arr[i].className=="checks" && arr[i].checked)
-                  {
-                      arr[i].checked=false;
-                  }
-              }
-          }
-      }
-</script>
+  </script>
+  <script src='adminAjax.js'></script>
 </head>
 <body onload="onLoad()">
   <?php require 'requiredHeader.php';?>
@@ -311,14 +88,17 @@
     <div id="sideBar">
       <div id="searchBox">
         <p><h3>Search for an event:</h3></p>
-        <input type="text"></input>
-        <button>Search</button>
+        <form action="searchEvents.php" method="POST">
+          <input type="search" name="keywords" />
+          <input type="submit" value="Search" />
+        </form>
         <br/><br/>
       </div>
       <div id="links">
         <a href="fcIndex.html" class="link">View Calendar</a><br/>
-        <a href="" class="link">View Organizations</a><br/>
-        <a href="" class="link">Edit Annoucements</a>
+        <!-- Links to trigger modals -->
+        <a data-toggle="modal" href="#announceModal" class="link">Add/Edit Announcements</a><br/>
+        <a data-toggle="modal" href="#orgListModal" class="link">View Organizations</a>
       </div>
       <br/>
       <hr/>
@@ -329,10 +109,67 @@
     </div>
   </div> 
   <div id="bottomWrapper">
-    <div id="testArea"></div>
     <footer>
       Created By: Matthew Castaldini, Hanan Jalnko, Kathleen Napier, Ian Tammis
     </footer> 
   </div>
+  
+<!-- announceModal -->
+<div id="announceModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- modal content -->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">LTU Announcements</h4>
+      </div>
+      <div class="modal-body">
+        <!-- modal panels -->
+	<ul class="nav nav-tabs" role="tablist">
+	<li role="presentation" class ="active"><a href="#add" aria-controls="add" role="tab" data-toggle="tab">Add Announcement</a></li>
+	<li role="presentation"><a href="#edit" aria-controls="edit" role="tab" data-toggle="tab">Edit Announcement</a></li>
+	</ul>
+        <div class="tab-content">
+          <!-- create new announcement -->
+          <div role="tabpanel" class="tab-pane active" id="add"><br />
+            <form action="announcements.php" method="POST" role="form">
+              <div class="form-group">
+                <textarea class="form-control" rows="6" id="addAnn" name="addAnn">Enter new announcement...</textarea>
+              </div>
+              <button type="submit" class="btn btn-default" name="source" value="admin.php">Add</button>
+            </form>
+          </div>
+          <!-- edit/delete current announcements -->
+          <div role="tabpanel" class="tab-pane" id="edit"><br />
+            <div id="modifyTable">
+              <?php modalTable(); ?>
+            </div> 
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- end of announceModal -->
+
+<!-- orgListModal -->
+<div id="orgListModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- modal content -->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Registered Organizations</h4>
+      </div>
+      <div class="modal-body">
+        <div id="allOrgs">
+          <?php buildOrgList(); ?> 
+        </div> 
+      </div>
+    </div>
+  </div>
+</div>
+<!-- end of orgListModal -->
+
 </body>
 </html>
