08dc23a6597a6f2c7585a012ff6e38d5762a843b
katnapier <knapier@ltu.edu>
2016-05-08 15:04:20 -0400

    New files for admin page
    Additional files for admin are below. You can look at them now, but I need to update the admin page before they will work.
    
    1.) adminAjax: External javascript file of variables and functions from the admin page
    2.) adminModalFunctions.php: two functions that helps build models for the announcements and organizations links
    3.) announcements.php: handles creating an new announcement (from the modal)
    2.) deleteAnnouncement.php: deletes an announcement (from the modal)
    3.) editAnnouncements.php: edits an announcement (from the modal)
    4.) searchEvents.php: simple search through only the evt_name column of ltuevents

---
+++ deleteAnnouncement.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body>
+<!-- Delete selected announcement -->
+<?php
+    $anId=intval($_POST['annId']);
+    $servername="localhost";
+    $username="root";
+    $password="root";
+    $dbname="LTUBillboard";
+
+    $con=new mysqli($servername, $username, $password, $dbname);
+    if($con->connect_error)
+    {
+        die("Connection failed:" . $con->connect_error);
+    }
+    $sql="DELETE FROM admin_announcements 
+          WHERE announceId=" . $anId;
+    if ($con->query($sql) === TRUE)
+    {
+        echo "Record successfully removed.";
+    } 
+    else
+    {
+	echo "Error: " . $sql . "<br>" . $con->error;
+    }
+    $con->close();
+    header("Location: admin.php");
+?>
+</body>
+</html>
