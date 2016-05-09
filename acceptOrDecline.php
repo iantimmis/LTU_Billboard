1d2391224f1cbaa35942d846de87b5300bac8992
katnapier <knapier@ltu.edu>
2016-05-09 17:59:06 -0400

    Update acceptOrDecline.php

--- acceptOrDecline.php
+++ acceptOrDecline.php
@@ -4,6 +4,7 @@
   <link href="fcStylesheet.css" rel="stylesheet" type="text/css" />
   <link href="adminStylesheet.css" rel="stylesheet" type="text/css" />
   <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <link rel="icon" href="favicon.ico"/>
 </head>
 <body>
   <?php
