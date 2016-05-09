e4e34b3e016f988db00479da8b9dddc746572a8e
mcastaldi <mcastaldi@ltu.edu>
2016-05-09 15:34:47 -0400

    Added checks for being admin
    Also got rid of ability to log in as org or create an account.

--- admin.php
+++ admin.php
@@ -1,257 +1,124 @@
 <?php
+	session_start();
     include 'upcomingEvents.php';
+	$thisPage = "admin.php";
+	
+	//Establish connection with the database
+	$servername = "localhost";
+	$dbusername = "root";
+	$dbpassword = "root";
+	$dbname = "LTUBillboard";
+	// Create connection
+	$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	$loginAttempted=false;
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		if(!empty($_POST['type'])){$type = $_POST['type'];}
+		if(strcmp($type,'stu')==0)//logging in as student
+		{
+			$loginAttempted = true;
+			if(empty($_POST['studentEmail']))
+				$emailErr = "Email is Required";
+			elseif(empty($_POST['studentPassword']))
+				$passwordErr = "Password is Required";
+			else
+			{
+				$email = cleanInput($_POST['studentEmail'],$conn);
+				$password = cleanInput($_POST['studentPassword'],$conn);
+				$sql = "SELECT * FROM user_account WHERE user_email='{$email}' AND login_password='{$password}';";
+				$result = $conn->query($sql);
+				if($result->num_rows==0){$loginMessage="Login Failed";}
+				else
+				{
+					$loginSuccess = true;
+					$userInfo = $result->fetch_assoc();
+					$_SESSION['userId'] = $userInfo['userId'];
+					$_SESSION["firstName"] = $userInfo['first_name'];
+					$_SESSION["lastName"] = $userInfo['last_name'];
+					$_SESSION['isAdmin'] = $userInfo['is_admin'];
+					$_SESSION['userEmail'] = $userInfo['user_email'];
+					$_SESSION['userPassword'] = $userInfo['login_password'];
+					$_SESSION['receiveEmails'] = $userInfo['receive_emails'];
+					$loginMessage = "Login Successful";
+				}
+			}
+		}
+	}
+	//check session to see if logged in and user and get info if true
+	$loggedInAsUser = $loggedInAsOrg = $loggedInAsAdmin = false;
+	if (isset($_SESSION['userId'])){
+		$userInfo['userId'] = $_SESSION['userId'];
+		$userInfo['firstName'] = $_SESSION["firstName"];
+		$userInfo['lastName'] = $_SESSION["lastName"];
+		$userInfo['isAdmin'] = $_SESSION['isAdmin'];
+		$userId = $userInfo['userId'];
+		$message  = $userInfo['firstName'] . " " . $userInfo['lastName'];
+		$loggedInAsUser = true;
+		$loggedInAsAdmin = true;
+	} elseif (isset($_SESSION['orgId'])) {
+		$orgInfo['id'] = $_SESSION['orgId'];
+		$orgInfo['name'] = $_SESSION['orgName'];
+		$orgInfo['desc'] = $_SESSION['orgDesc'];
+		$orgInfo['website'] = $_SESSION['orgWebsite'];
+		$loggedInAsOrg = true;
+		$message = $orgInfo['name'];
+	} else {
+		$message = "No One";
+	}
+	$loggedIn = $loggedInAsOrg || $loggedInAsUser;
+	function cleanInput($input,$conn){//function for cleaning input
+		$input = trim($input);
+		$input = stripslashes($input);
+		$input = htmlspecialchars($input);
+		$input = mysqli_real_escape_string($conn,$input);
+      	return $input;
+	}
+	$conn->close();
 ?>
+
+<?php require 'adminModalFunctions.php';?>
 
 <!DOCTYPE html>
 <html>
 <head>
-  <link href="fcStylesheet.css" rel="stylesheet" type="text/css" />
-  <link href="adminStylesheet.css" rel="stylesheet" type="text/css" />
+  <link href="stylesheet.css" rel="stylesheet" type="text/css" />
   <link href="adminStylesheet.css" rel="stylesheet" type="text/css" />
   <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <link href="bootstrap.css" rel="stylesheet" type="text/css">
+	<script type="text/javascript" src="jquery-2.2.2.min.js"></script>
+	<script src='jquery.min.js'></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
+	<script src='moment.min.js'></script>
+	<script src='fullcalendar.js'></script>
+	<script type="text/javascript" src="bootstrap.min.js"></script>
   <script>
-      var currPage=0
-      var maxPage=0
-      var sortBy=0
-      var type=0
-      function onLoad()
-      {
-          baseFunction("http://localhost/ltu_billboard/findMax.php", findMaxPage);
-          baseFunction("http://localhost/ltu_billboard/buildRequestTable.php?page="+currPage+"&sort="+sortBy+"&type="+type, showRequests);
-      }
-      function next()
-      {
-          if(currPage==maxPage)
-          {
-              //do nothing
-          }
-          else
-          {
-              currPage++;
-              baseFunction("http://localhost/ltu_billboard/buildRequestTable.php?page="+currPage+"&sort="+sortBy+"&type="+type, showRequests);
-          }
-      }
-      function prev()
-      {
-          if(currPage==0)
-          {
-              //do nothing
-          }
-          else
-          {
-              currPage--;
-              baseFunction("http://localhost/ltu_billboard/buildRequestTable.php?page="+currPage+"&sort="+sortBy+"&type="+type, showRequests);
-          }
-      }
-      function sortByRequestDate()
-      {
-          if(sortBy==0)
-          {
-              //do nothing
-          }
-          else
-          {
-              sortBy=0;
-
-              document.getElementById("sortDate").style.backgroundColor="rgba(255, 255, 255, .3)";
-              document.getElementById("sortDate").style.color="#003d78";
-              document.getElementById("sortRecent").style.backgroundColor="#3385ff";
-              document.getElementById("sortRecent").style.color="white";
-
-              baseFunction("http://localhost/ltu_billboard/buildRequestTable.php?page="+0+"&sort="+sortBy+"&type="+type, showRequests);
-          }
-        
-      }
-      function sortByEventDate()
-      {
-          if(sortBy==1)
-          {
-              //do nothing
-          }
-          else
-          {
-              sortBy=1;
-
-              document.getElementById("sortRecent").style.backgroundColor="rgba(255, 255, 255, .3)";
-              document.getElementById("sortRecent").style.color="#003d78";
-              document.getElementById("sortDate").style.backgroundColor="#3385ff";
-              document.getElementById("sortDate").style.color="white";
-
-              baseFunction("http://localhost/ltu_billboard/buildRequestTable.php?page="+0+"&sort="+sortBy+"&type="+type, showRequests);
-          }
-      }
-      function showEventRequests()
-      {
-          if(type==0)
-          {
-              //do nothing
-          }
-          else
-          {
-              type=0;
-
-              document.getElementById("orgReq").style.backgroundColor="rgba(255, 255, 255, .3)";
-              document.getElementById("orgReq").style.color="#003d78";
-              document.getElementById("eventReq").style.backgroundColor="#3385ff";
-              document.getElementById("eventReq").style.color="white";
-
-              
-              document.getElementById("sortDate").style.backgroundColor="rgba(255, 255, 255, .3)";
-              document.getElementById("sortDate").style.color="#003d78";
-              document.getElementById("sortDate").style.display="initial";
-              document.getElementById("sortRecent").style.backgroundColor="#3385ff";
-              document.getElementById("sortRecent").style.color="white";
-              
-
-              baseFunction("http://localhost/ltu_billboard/buildRequestTable.php?page="+0+"&sort="+sortBy+"&type="+type, showRequests);
-          }
-      }
-      function showOrgRequests()
-      {
-          if(type==1)
-          {
-              //do nothing
-          }
-          else
-          {
-              type=1;
-
-              document.getElementById("eventReq").style.backgroundColor="rgba(255, 255, 255, .3)";
-              document.getElementById("eventReq").style.color="#003d78";
-              document.getElementById("orgReq").style.backgroundColor="#3385ff";
-              document.getElementById("orgReq").style.color="white";
-
-              document.getElementById("sortDate").style.display="none";
-              document.getElementById("sortRecent").style.backgroundColor="#3385ff";
-              document.getElementById("sortRecent").style.color="white";
-
-              baseFunction("http://localhost/ltu_billboard/buildRequestTable.php?page="+0+"&sort="+sortBy+"&type="+type, showRequests);
-          }
-      }
-      function baseFunction(url, nameOfFunction)
-      {
-          var xhttp=new XMLHttpRequest();
-          xhttp.onreadystatechange=
-              function()
-              {
-                  if(xhttp.readyState==4 && xhttp.status==200)
-                  {
-                      nameOfFunction(xhttp);
-                  }
-              };
-              xhttp.open("GET", url, true);
-              xhttp.send();
-      }
-      function baseFunctionPost(url, nameOfFunction, msg)
-      {
-          var xhttp=new XMLHttpRequest();
-          xhttp.onreadystatechange=
-              function()
-              {
-                  if(xhttp.readyState==4 && xhttp.status==200)
-                  {
-                      nameOfFunction(xhttp);
-                  }
-              };
-              xhttp.open("POST", url, true);
-              xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-              xhttp.send(msg);
-      }
-      function findMaxPage(xhttp)
-      {
-          maxPage=xhttp.responseText;   
-      }
-      function showRequests(xhttp)
-      {
-          document.getElementById("requestTable").innerHTML=xhttp.responseText;        
-      }
-      function handleAcceptance(xhttp)
-      {
-          document.getElementById("acceptanceResponse").innerHTML=xhttp.responseText;
-      }
-      function accept(acc)
-      {
-          var arr=document.getElementsByTagName("INPUT");  
-          var checkedIt=new Array();
-          for(var i=0; i<arr.length; i++)
-          {
-              if(arr[i].className=="checks" && arr[i].checked)
-              {
-                  var rowID=arr[i].id;
-                  checkedIt.push(rowID);
-              }
-          }
-          if(checkedIt==="undefined" || checkedIt.length==0)
-          {
-              return;
-          }
-          if(acc==0)
-          {
-              var decision=confirm("Are you sure you want to delete request(s)?");
-              if(!decision)
-              {
-	          return;
-              }	
-          }
-          else
-          {
-              var decision=confirm("Are you sure you want to accpet request(s)?");
-              if(!decision)
-              {
-	          return;
-              }	
-          }
-          var checkedItems=new Array();
-          for(var j=0; j<checkedIt.length; j++)
-          {
-              /*var val=0;*/
-              var val=document.getElementById("reqs").rows[checkedIt[j]].cells[0].innerHTML;
-              checkedItems.push(val);
-          }
-          var data="a="+acc+"&type="+type+"&data="+checkedItems;
-          baseFunctionPost("http://localhost/ltu_billboard/acceptOrDecline.php", handleAcceptance, data); 
-          baseFunction("http://localhost/ltu_billboard/buildRequestTable.php?page="+currPage+"&sort="+sortBy+"&type="+type, showRequests); 
-      }
-      function changeAll()
-      {
-          var arr=document.getElementsByTagName("INPUT");       
-          if(document.getElementById("masterCheck").checked)  
-          {			
-              for(var i=0; i<arr.length; i++)
-              {
-                  if(arr[i].className=="checks" && !arr[i].checked)
-                  {
-                      arr[i].checked=true;
-                  }
-              }
-          }
-          else 
-          {
-              for(var i=0; i<arr.length; i++)
-              {
-                  if(arr[i].className=="checks" && arr[i].checked)
-                  {
-                      arr[i].checked=false;
-                  }
-              }
-          }
-      }
-</script>
+		$(document).ready(function() {
+			$('#loginTabs a:last').hide();
+			$('#loginTabs a[href="#loginAsOrg"]').hide();
+			$('#createAccountLink').hide();
+			<?php if($loginAttempted):?>
+				<?php if(strcmp($type,'stu')==0):?>
+					<?php if(!$loginSuccess):?>
+					//login student fail
+					$('#loginModal').modal('show');
+					$('#stuLoginMessage').html("Login Failed");
+					$('#stuLoginMessage').toggleClass('error');
+					<?php endif; ?>
+				<?php endif; ?>
+			<?php endif; ?>
+			});
+  </script>
+  <?php if($loggedInAsAdmin):?>
+  <script src='adminAjax.js'></script>
+  <?php endif;?>
 </head>
-<body onload="onLoad()">
-  <header>
-    <a href="create_events_page.htm"><button class="event">Create Event</button></a>
-    <a href="fcIndex.html" id=logo>LTU Billboard</a>
-    <span class="log-in">
-      <a href=""><button id="loginButton">Log In</button></a>
-      <br/>
-      Not a user? <a href="">Join Now</a>
-    </span>
-    <br/>
-    <br/>
-    <br/>
-  </header>
+<body <?php if($loggedInAsAdmin){echo "onload='onLoad()'";}?>>
+  <?php require 'requiredHeader.php';?>
   <div id="mainWrap">
+  <?php if($loggedInAsAdmin):?>
     <div id="title">Administrator</div>
     <div id="acceptanceResponse"></div>
     <div id="type">Current Requests</div>
@@ -284,14 +151,17 @@
     <div id="sideBar">
       <div id="searchBox">
         <p><h3>Search for an event:</h3></p>
-        <input type="text"></input>
-        <button>Search</button>
+        <form action="searchEvents.php" method="POST">
+          <input type="search" name="keywords" />
+          <input type="submit" value="Search" />
+        </form>
         <br/><br/>
       </div>
       <div id="links">
-        <a href="fcIndex.html" class="link">View Calendar</a><br/>
-        <a href="" class="link">View Organizations</a><br/>
-        <a href="" class="link">Edit Annoucements</a>
+        <a href="index.php" class="link">View Calendar</a><br/>
+        <!-- Links to trigger modals -->
+        <a data-toggle="modal" href="#announceModal" class="link">Add/Edit Announcements</a><br/>
+        <a data-toggle="modal" href="#orgListModal" class="link">View Organizations</a>
       </div>
       <br/>
       <hr/>
@@ -301,8 +171,67 @@
       </div>
     </div>
   </div> 
+  
+<!-- announceModal -->
+<div id="announceModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- modal content -->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">LTU Announcements</h4>
+      </div>
+      <div class="modal-body">
+        <!-- modal panels -->
+	<ul class="nav nav-tabs" role="tablist">
+	<li role="presentation" class ="active"><a href="#add" aria-controls="add" role="tab" data-toggle="tab">Add Announcement</a></li>
+	<li role="presentation"><a href="#edit" aria-controls="edit" role="tab" data-toggle="tab">Edit Announcement</a></li>
+	</ul>
+        <div class="tab-content">
+          <!-- create new announcement -->
+          <div role="tabpanel" class="tab-pane active" id="add"><br />
+            <form action="announcements.php" method="POST" role="form">
+              <div class="form-group">
+                <textarea class="form-control" rows="6" id="addAnn" name="addAnn">Enter new announcement...</textarea>
+              </div>
+              <button type="submit" class="btn btn-default" name="source" value="admin.php">Add</button>
+            </form>
+          </div>
+          <!-- edit/delete current announcements -->
+          <div role="tabpanel" class="tab-pane" id="edit"><br />
+            <div id="modifyTable">
+              <?php modalTable(); ?>
+            </div> 
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- end of announceModal -->
+
+<!-- orgListModal -->
+<div id="orgListModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- modal content -->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Registered Organizations</h4>
+      </div>
+      <div class="modal-body">
+        <div id="allOrgs">
+          <?php buildOrgList(); ?> 
+        </div> 
+      </div>
+    </div>
+  </div>
+</div>
+<!-- end of orgListModal -->
+	<?php else: ?>
+	<h1>Need to be logged in as an administrator to view this page</h1>
+<?php endif;?>
   <div id="bottomWrapper">
-    <div id="testArea"></div>
     <footer>
       Created By: Matthew Castaldini, Hanan Jalnko, Kathleen Napier, Ian Timmis
     </footer> 
